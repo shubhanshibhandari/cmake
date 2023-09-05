4fd498d85f66507fb3657f3e9b8b87f36c960620
shubhanshibhandari <shubhanshibhandari111@gmail.com>
2023-09-05 23:35:14 +0530

    tutorial_1

---
+++ out/build/CMakeFiles/cmake_tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_tutorial.dir/main.cpp.o"
+  "CMakeFiles/cmake_tutorial.dir/main.cpp.o.d"
+  "cmake_tutorial"
+  "cmake_tutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
