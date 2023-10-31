85254f7f0ec09a32b6c5347307c16c636039cac2
shubhanshibhandari <shubhanshibhandari111@gmail.com>
2023-10-31 17:54:35 +0530

    tutorial 1 and tutorial2

---
+++ tut2/somelibDemo/cpp/build/CMakeFiles/mearlymath.dir/adder.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/mearlymath.dir/adder.cpp.o: \
+ /home/kakshi/Desktop/ProjectsAndCodes/cmake/tut2/somelibDemo/cpp/adder.cpp \
+ /usr/include/stdc-predef.h \
+ /home/kakshi/Desktop/ProjectsAndCodes/cmake/tut2/somelibDemo/cpp/adder.h
